1542ce8684de2c34cdecaecaea6373e1eb5f0670
JesusT <core.jesust@gmail.com>
2020-09-10 13:03:35 +0200

    App funcional. Carga web en webview de otra actividad

--- app/src/main/java/com/example/verweb/MainActivity.java
+++ app/src/main/java/com/example/verweb/MainActivity.java
@@ -5,17 +5,40 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText webToShow;
+    private String uriWeb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        webToShow = findViewById(R.id.url);
     }
 
-    public void openActivityShowWeb(View view){
-        Intent activInfo = new Intent(this, ShowWebActivity.class);
-        startActivity(activInfo);
+    public void goTo(View view){
+        TextView advertencia = (TextView) findViewById(R.id.advertencia);
+        this.uriWeb = this.webToShow.getText().toString();
+        if (uriWeb.isEmpty()) {
+            advertencia.setVisibility(View.VISIBLE);
+        } else {
+            advertencia.setVisibility(View.INVISIBLE);
+            openActivityShowWeb();
+        }
+    }
+
+    private void openActivityShowWeb(){
+        Intent activWebShow = new Intent(this, ShowWebActivity.class);
+        activWebShow.putExtra("direccion", this.uriWeb);
+        startActivity(activWebShow);
+    }
+
+    public void closeApp(View view){
+        finish();
     }
 }
--- app/src/main/java/com/example/verweb/ShowWebActivity.java
+++ app/src/main/java/com/example/verweb/ShowWebActivity.java
@@ -4,14 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class ShowWebActivity extends AppCompatActivity {
+
+    String web;
+    WebView myWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_web);
+        myWebView = (WebView) findViewById(R.id.webView);
+        Bundle bundle = getIntent().getExtras();
+            if (bundle != null) {
+                this.web = bundle.getString("direccion");
+            } else {
+                this.web = "google.es";
+            }
+        myWebView.setWebViewClient(new WebViewClient());
+        myWebView.loadUrl("http://"+web);
     }
+
 
     public void close(View view) {
         finish();
